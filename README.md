cbd90855d2421592be641b158c6282e40013caf6
Jairo Serrano <jairo.serrano@gmail.com>
2015-08-27 17:00:07 -0500

    Se integra una plantilla más vistosa y se modifica las plantillas

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # DemoMicroBlog
 Demo de un servicio de microblogging para Diseño de Ambientes Web 3, Ingeniería de Sistemas, Universidad Tecnológica de Bolívar 
+
+## Plantilla usada:
+Start Bootstrap was created by and is maintained by **David Miller**, Managing Parter at [Iron Summit Media Strategies](http://www.ironsummitmedia.com/).
+ * https://twitter.com/davidmillerskt
+ * https://github.com/davidtmiller
+Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
--- resources/views/microblog/index.blade.php
+++ resources/views/microblog/index.blade.php
@@ -1,53 +1,176 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Ejemplo basado en documentación de Laravel, Bootstrap y Laracast">
-    <meta name="author" content="Jairo Serrano">
-    <link rel="icon" href="favicon.ico">
-    <title>Narrow Jumbotron Template for Bootstrap</title>
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet">
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
-    <!-- Custom styles for this template -->
-    <link href="/css/app.css" rel="stylesheet">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Freelancer - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS - Uses Bootswatch Flatly Theme: http://bootswatch.com/flatly/ -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/css/freelancer.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
-<body>
+<body id="page-top" class="index">
 
-<div class="container">
-    <div class="header clearfix">
-        <nav>
-            <ul class="nav nav-pills pull-right">
-                <li role="presentation" class="active"><a href="/">Home</a></li>
-                <li role="presentation"><a href="/all.json">JSON data</a></li>
-                <li role="presentation"><a href="https://github.com/jairoserrano/DemoMicroBlog">Proyecto en Github</a></li>
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#page-top">Microblog demo</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="hidden">
+                    <a href="#page-top"></a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/">Inicio</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="/all.json">Datos en Json</a>
+                </li>
+                <li class="page-scroll">
+                    <a href="https://github.com/jairoserrano/DemoMicroBlog">Código en Github</a>
+                </li>
             </ul>
-        </nav>
-        <h3 class="text-muted">Project name</h3>
+        </div>
+        <!-- /.navbar-collapse -->
     </div>
+    <!-- /.container-fluid -->
+</nav>
 
-    @forelse ($posts as $post)
-    <div>
-        <h1>{{ $post->title }}</h1>
-        <small>{{ $post->created_at }}</small>
-        <p>{{ $post->body }}</p>
+<!-- Header -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <img class="img-responsive" src="/img/profile.png" alt="">
+                <div class="intro-text">
+                    <span class="name">Bienvenidos</span>
+                    <hr class="star-light">
+                    <span class="skills">Laravel 5.1 - Bootstrap 3</span>
+                </div>
+            </div>
+        </div>
     </div>
-    @empty
-        <p>No hay nada por ahora...</p>
-    @endforelse
+</header>
 
-    <footer class="footer">
-        <p>&copy; Company 2014</p>
-    </footer>
+<!-- Portfolio Grid Section -->
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2>Mensajes</h2>
+                <hr class="star-primary">
+            </div>
+        </div>
+        @forelse ($posts as $post)
+        <div class="row">
+            <div class="col-sm-12 portfolio-item">
+                <h1>{{ $post->title }}</h1>
+                <small>{{ $post->created_at }}</small>
+                <p>{{ $post->body }}</p>
+            </div>
+        </div>
+        @empty
+            <p>No hay nada por ahora...</p>
+        @endforelse
+    </div>
+</section>
 
-</div> <!-- /container -->
-<!-- Jquery and Bootstrap JS libraries from CDN -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<!-- Footer -->
+<footer class="text-center">
+    <div class="footer-above">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col col-md-4">
+                    <h3>Dirección</h3>
+                    <p>Barrio Ternera<br>KM 1 vía Turbaco, Parque industrial y tecnológico Carlos Velez Pombo</p>
+                </div>
+                <div class="footer-col col-md-4">
+                    <h3>UTB en la Web</h3>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="https://www.facebook.com/UNITECNOLOGICA" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="http://www.unitecnologica.edu.co" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-col col-md-4">
+                    <h3>Acerca de este proyecto</h3>
+                    <p>Este proyecto es completamente libre de usar, estudiar y adaptar.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-below">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    Ingeniería de Sistemas - Universidad Tecnológica de Bolívar - 2015
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+<div class="scroll-top page-scroll visible-xs visible-sm">
+    <a class="btn btn-primary" href="#page-top">
+        <i class="fa fa-chevron-up"></i>
+    </a>
+</div>
+
+<!-- jQuery -->
+<script src="/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+<script src="/js/classie.js"></script>
+<script src="/js/cbpAnimatedHeader.js"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="/js/jqBootstrapValidation.js"></script>
+<script src="/js/contact_me.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="/js/freelancer.js"></script>
+
 </body>
+
 </html>
